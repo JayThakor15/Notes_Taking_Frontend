44aa200cef10b346b7ef4ca2b75d8a73300d8970
Parth Khatri <parthkhatri0987@gmail.com>
2025-08-31 11:35:51 +0530

    Added Backend Deplyoed Link

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
+// const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://notes-taking-backend-5ewv.onrender.com/api";
 
 const API = axios.create({
   baseURL: BASE_URL,
